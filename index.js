c19f3fbfd2ad9e8099089e2c71045f67225e1f91
Андрей <andrevyakin@gmail.com>
2022-12-06 01:49:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,201 @@
+const tasks = [
+    {
+        id: "1138465078061",
+        completed: false,
+        text: "Посмотреть новый урок по JavaScript",
+    },
+    {
+        id: "1138465078062",
+        completed: false,
+        text: "Выполнить тест после урока",
+    },
+    {
+        id: "1138465078063",
+        completed: false,
+        text: "Выполнить ДЗ после урока",
+    },
+];
+
+const elementFactory = (element, parameters) => {
+    const el = document.createElement(element);
+    if (parameters.className)
+        Array.isArray(parameters.className)
+            ? parameters.className.forEach(i => el.classList.add(i))
+            : el.className = parameters.className;
+    if (parameters.dataset)
+        el.dataset[parameters.dataset.name] = parameters.dataset.value;
+    if (parameters.type)
+        el.type = parameters.type;
+    if (parameters.id)
+        el.id = parameters.id;
+    if (parameters.htmlFor)
+        el.htmlFor = parameters.htmlFor;
+    if (parameters.text)
+        el.textContent = parameters.text;
+    return el;
+}
+
+const createItems = (id, text) => {
+    const taskItem = elementFactory("div", {
+        className: "task-item",
+        dataset: {
+            name: "taskId",
+            value: id
+        }
+    });
+    const mainContainer = elementFactory("div", {
+        className: "task-item__main-container"
+    });
+    taskItem.append(mainContainer);
+    const mainContent = elementFactory("div", {
+        className: "task-item__main-content"
+    });
+    mainContainer.append(mainContent);
+    const form = elementFactory("form", {
+        className: "task-item__form"
+    });
+    mainContent.append(form);
+    const input = elementFactory("input", {
+        className: "checkbox-form__checkbox",
+        type: "checkbox",
+        id: `task-${id}`
+    });
+    form.append(input);
+    const label = elementFactory("label", {
+        htmlFor: `task-${id}`
+    });
+    form.append(label);
+    const span = elementFactory("span", {
+        className: "task-item__text",
+        text
+    });
+    mainContent.append(span);
+    const button = elementFactory("button", {
+        className: ["task-item__delete-button", "default-button", "delete-button"],
+        dataset: {
+            name: "deleteTaskId",
+            value: id
+        },
+        text: "Удалить"
+    });
+    mainContainer.append(button);
+    return taskItem;
+}
+
+let flagLightOrDark = false;
+const lightOrDarkTheme = flag => {
+    flag
+        ? document.body.style.background = "#24292E"
+        : document.body.style.background = "initial";
+    document.querySelectorAll(".task-item")
+        .forEach(el => flag
+            ? el.style.color = "#fff"
+            : el.style.color = "initial");
+    document.querySelectorAll("button")
+        .forEach(button => flag
+            ? button.style.border = "1px solid #fff"
+            : button.style.border = "none");
+}
+document.addEventListener("keydown", event => {
+    if (event.key === "Tab") {
+        flagLightOrDark = !flagLightOrDark;
+        lightOrDarkTheme(flagLightOrDark);
+    }
+})
+
+const tasksList = document.querySelector(".tasks-list");
+const addItems = () => {
+    tasks.forEach(task => tasksList.append(createItems(task.id, task.text)));
+    lightOrDarkTheme(flagLightOrDark);
+}
+addItems();
+
+const createTaskBlock = document.querySelector(".create-task-block");
+const spanError = document.createElement("span");
+
+const removeErrorMessage = () => {
+    document.querySelector(".error-message-block")
+        ? spanError.remove()
+        : spanError.className = "error-message-block";
+}
+
+const validations = text => {
+    removeErrorMessage();
+    if (!text || tasks.some(i => i.text === text)) {
+        !text
+            ? spanError.textContent = "Название задачи не должно быть пустым."
+            : spanError.textContent = "Задача с таким названием уже существует."
+        createTaskBlock.prepend(spanError);
+        return false;
+    }
+    return true;
+}
+
+createTaskBlock.addEventListener("submit", event => {
+    event.preventDefault();
+    const textToAdd = event.target.taskName.value
+    if (validations(textToAdd)) {
+        tasks.unshift({id: `${Date.now()}`, completed: false, text: textToAdd});
+        tasksList.prepend(createItems(tasks[0].id, textToAdd));
+        lightOrDarkTheme(flagLightOrDark);
+    }
+})
+
+const createModalWindow = () => {
+    const modalOverlay = elementFactory("div", {
+        className: ["modal-overlay", "modal-overlay_hidden"]
+    });
+    const deleteModal = elementFactory("div", {
+        className: "delete-modal"
+    });
+    modalOverlay.append(deleteModal);
+    const question = elementFactory("h3", {
+        className: "delete-modal__question",
+        text: "Вы действительно хотите удалить эту задачу?"
+    });
+    deleteModal.append(question);
+    const divButtons = elementFactory("div", {
+        className: "delete-modal__buttons"
+    });
+    deleteModal.append(divButtons);
+    const cancelButton = elementFactory("button", {
+        className: ["delete-modal__button", "delete-modal__cancel-button"],
+        text: "Отмена"
+    });
+    divButtons.append(cancelButton);
+    const confirmButton = elementFactory("button", {
+        className: ["delete-modal__button", "delete-modal__confirm-button"],
+        text: "Удалить"
+    })
+    divButtons.append(confirmButton);
+    return modalOverlay;
+}
+
+const modalWindow = createModalWindow();
+document.body.appendChild(modalWindow);
+let isDeleteButton;
+tasksList.addEventListener("click", event => {
+    isDeleteButton = event.target.closest(".delete-button");
+    if (isDeleteButton) {
+        removeErrorMessage();
+        modalWindow.classList.toggle("modal-overlay_hidden");
+    }
+})
+
+const deleteModalButtons = document.querySelector(".delete-modal__buttons");
+deleteModalButtons.addEventListener("click", event => {
+    const isButton = event.target.closest(".delete-modal__button");
+    if (isButton && event.target.classList.contains("delete-modal__cancel-button"))
+        modalWindow.classList.toggle("modal-overlay_hidden");
+    if (isButton && event.target.classList.contains("delete-modal__confirm-button")) {
+        for (let i = 0; i < tasks.length; i++)
+            if (tasks[i].id === isDeleteButton.dataset.deleteTaskId) {
+                tasks.splice(i, 1);
+                break;
+            }
+        modalWindow.classList.toggle("modal-overlay_hidden");
+        tasksList.replaceChildren();
+        tasks.forEach(i => tasksList.append(createItems(i.id, i.text)));
+        lightOrDarkTheme(flagLightOrDark);
+    }
+})
